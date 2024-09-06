e0de5018e7be1ff6f3d1490e2d3c5968f6decd99
Cologler <skyoflw@gmail.com>
2024-09-06 16:37:37 +0800

    feat: support regex match

--- prunefiles/app.py
+++ prunefiles/app.py
@@ -5,6 +5,7 @@
 # 
 # ----------
 
+import re
 from functools import cached_property
 from pathlib import Path
 from typing import Annotated, Any
@@ -13,6 +14,28 @@ import humanfriendly
 import parse
 import rich
 import typer
+
+
+class ParseMatcher:
+    def __init__(self, format: str, case_sensitive: bool) -> None:
+        self.__parser = parse.compile(format, case_sensitive=case_sensitive)
+
+    def match(self, value: str) -> parse.Result | None:
+        return self.__parser.parse(value)
+
+    def get_value(self, match_result: parse.Result, name: str) -> Any:
+        return match_result[name]
+
+
+class RegexMatcher:
+    def __init__(self, pattern: str, case_sensitive: bool) -> None:
+        self.__pattern = re.compile(pattern, re.IGNORECASE if not case_sensitive else 0)
+
+    def match(self, value: str) -> re.Match | None:
+        return self.__pattern.fullmatch(value)
+
+    def get_value(self, match_result: re.Match, name: str) -> Any:
+        return match_result.group(name)
 
 
 class _PathState:
@@ -35,10 +58,18 @@ def prune_files(
             Path,
             typer.Argument(exists=True, file_okay=False, dir_okay=True, resolve_path=True),
         ],
-        match_format: Annotated[str, typer.Option(help="match format, alsosee https://github.com/r1chardj0n3s/parse.")] = None,
+
+        match_format: Annotated[str, typer.Option(
+            help="match format, alsosee https://github.com/r1chardj0n3s/parse.")] = None,
+        match_regex: Annotated[str, typer.Option(
+            help="match regex, alsosee https://docs.python.org/3/library/re.html#regular-expression-syntax.")] = None,
+        match_case_sensitive: Annotated[bool, typer.Option('--match-case-sensitive',
+            help='match case sensitive, default is case-insensitive.')] = False,
+
         orderby: Annotated[str, typer.Option(help='captured from --match-format. leave empty to sort by name.')] = None,
         keep_count: Annotated[int, typer.Option(help='The count of files to keep.')] = None,
         keep_size: Annotated[str, typer.Option(help='The max size of files to keep.')] = None,
+
         dry_run: Annotated[bool, typer.Option('--dry-run')] = False,
     ):
 
@@ -56,6 +87,18 @@ def prune_files(
             rich.print('keep-size must be a valid size')
             raise typer.Exit(1)
 
+    if sum(int(bool(x)) for x in [match_regex, match_format]) > 1:
+        rich.print('Only one of --match-format or --match-regex can be specified')
+        raise typer.Exit(1)
+    elif match_format:
+        rich.print(f'Using --match-format: [green]{match_format}[/]')
+        matcher = ParseMatcher(match_format, match_case_sensitive)
+    elif match_regex:
+        rich.print(f'Using --match-regex: [green]{match_regex}[/]')
+        matcher = RegexMatcher(match_regex, match_case_sensitive)
+    else:
+        matcher = None
+
     # print execute info
     rich.print(f"Pruning files in [green]{folder}[/]")
 
@@ -65,11 +108,11 @@ def prune_files(
     # filter
     files.sort(key=lambda x: x.path.name)
 
-    if match_format:
+    if matcher:
         for file in files:
-            if match := parse.parse(match_format, file.path.name):
+            if match := matcher.match(file.path.name):
                 if orderby:
-                    file.orderby = match.named[orderby]
+                    file.orderby = matcher.get_value(match, orderby)
             file.is_match = bool(match)
 
     if excluded := [x for x in files if not x.is_match]:
